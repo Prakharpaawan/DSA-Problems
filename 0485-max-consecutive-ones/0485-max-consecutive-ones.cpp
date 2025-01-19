7efd8cc47d9f8e1bca36f630adf0151eccaf47f8
Prakhar <prakharpaawan@gmail.com>
2025-01-19 12:48:02 +0530

    Time: 0 ms (100%), Space: 50.1 MB (83.14%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,7 +5,7 @@ public:
         int maxcount = 0;
         for(int i=0;i<nums.size();i++) {
              if(nums[i]==1) {
-                count ++;
+                count +=1;
                 maxcount = max(maxcount, count);
              }
              else {
