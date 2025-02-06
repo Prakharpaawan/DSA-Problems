65b3caa38d8ac5f8b3f1ac1776eacbc97dc0c7cb
Prakhar <prakharpaawan@gmail.com>
2025-02-06 20:09:40 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,39 +1,23 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        vector<bool> row(n, false); // Mark rows to be zeroed
-        vector<bool> col(m, false); // Mark columns to be zeroed
 
-        // Step 1: Identify rows and columns that need to be zeroed
+        vector<int> row(n, 0);
+        vector<int> col(m,0);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+            for (int j = 0; j < m; j++){
+                if(matrix[i][j]==0) {
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
-
-        // Step 2: Set marked rows to zero
-        for (int i = 0; i < n; i++) {
-            if (row[i]) {
-                for (int j = 0; j < m; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Step 3: Set marked columns to zero
-        for (int j = 0; j < m; j++) {
-            if (col[j]) {
-                for (int i = 0; i < n; i++) {
-                    matrix[i][j] = 0;
+        for(int i=0;i<n;i++) {
+            for( int j=0;j<m;j++){
+                if(row[i]||col[j]){
+                    matrix[i][j]=0;
                 }
             }
         }
