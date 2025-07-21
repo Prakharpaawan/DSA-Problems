cb514770d13bbb690313deaf5999d180cd7153c0
Prakhar <prakharpaawan@gmail.com>
2025-07-21 05:57:07 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.82%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+
+        string doubled = s + s;
+        int pos = doubled.find(goal);  
+        
+        if (pos == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
